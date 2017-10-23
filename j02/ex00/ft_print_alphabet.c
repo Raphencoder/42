9a2c4d1212ec297d8cff4005df22105726a952d2
RaphaelKrief <sachagopok@gmail.com>
2017-10-23 12:50:42 +0200

    ajout

---
+++ j02/ex00/ft_print_alphabet.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rkrief <raphaelkriefbm@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/07 18:44:56 by rkrief            #+#    #+#             */
+/*   Updated: 2017/08/21 23:48:56 by rkrief           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+		write(1, &c, 1);
+		return ;
+}
+
+void	ft_print_alphabet(void)
+{
+	int	i;
+	
+	i = 'a';
+		while (i <= 'z')
+		{
+				ft_putchar(i);
+				i++;
+		}
+}
+
+int		main(void)
+{
+		ft_print_alphabet();
+		return (0);
+}
+
