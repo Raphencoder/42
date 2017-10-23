3d8985f714989adeaa10236700807d0e47138212
RaphaelKrief <sachagopok@gmail.com>
2017-10-23 12:55:32 +0200

    maj du ft_ dans le prototype

--- libft/ft_strcat.c
+++ libft/ft_strcat.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*strcat(char *dest, const char *src)
+char	ft_strcat(char *dest, const char *src)
 {
 	int	i;
 	int	j;
